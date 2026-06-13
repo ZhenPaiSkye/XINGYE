794e5415bc475c25568916a05b47ae8172e32635
ZhenPaiSkye <1053703868@qq.com>
2026-06-13 20:08:58 +0800

    增加了2个api

--- Quasar.Server/Extensions/HttpApi/HttpAllApi.cs
+++ Quasar.Server/Extensions/HttpApi/HttpAllApi.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using Quasar.Common.Enums;
+using Quasar.Common.Messages;
 using Quasar.Server.Messages;
 using Quasar.Server.Models;
 using Quasar.Server.Networking;
@@ -13,10 +15,15 @@ namespace Quasar.Server.Extensions.HttpApi
     {
         private QuasarServer _listenServer = QuasarServer.Instance;
 
+        /// <summary>
+        /// 获取所有客户端信息
+        /// </summary>
         [HttpGet("/get_clis")]
         public string GetClients(HttpListenerRequest request)
         {
+            //获取所有客户端
             Client[] clients = _listenServer.ConnectedClients;
+            //客户端的数量
             int total = clients.Length;
 
             var clientList = new List<object>();
@@ -41,10 +48,15 @@ namespace Quasar.Server.Extensions.HttpApi
             return response;
         }
 
+        /// <summary>
+        /// 指定客户端执行cmd命令
+        /// </summary>
         [HttpPost("/send_command")]
         public string PostSendCommend(HttpListenerRequest request)
         {
+
             string response = HttpResponse.Build(-1, "err", null);
+            //解析post参数
             using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
             {
                 string jsonData = reader.ReadToEnd();
@@ -53,24 +65,93 @@ namespace Quasar.Server.Extensions.HttpApi
                 var data = JsonConvert.DeserializeObject<dynamic>(jsonData);
 
                 string commend = data.commend;
-                string clientId = data.clientId;
+                List<string> ids = data.id.ToObject<List<string>>();
 
+                //获取所有客户端
                 Client[] clients = _listenServer.ConnectedClients;
-                foreach (Client c in clients)
-                {
-                    if (c.Value?.Id == clientId)
-                    {
-                        new RemoteShellHandler(c).SendCommand(commend);
-                        response = HttpResponse.Build(0, "ok", null);
-                        break;
-                    }
-                }
 
-                //response = HttpResponse.Build(1, "no", "没有找到客户端");
+                //执行命令
+                foreach (string id in ids)
+                {
+                    foreach (Client c in clients)
+                    {
+                        if (c.Value?.Id == id)
+                        {
+                            new RemoteShellHandler(c).SendCommand(commend);
+                            break;
+                        }
+                    }
+
+                    response = HttpResponse.Build(0, "ok", null);
+                }
             }
 
 
             return response;
         }
+
+        /// <summary>
+        /// 指定客户端执行电源操作
+        /// </summary>
+        [HttpPost("/power_control")]
+        public string PowerControl(HttpListenerRequest request)
+        {
+            string response = HttpResponse.Build(-1, "err", null);
+            //解析post参数
+            using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                string jsonData = reader.ReadToEnd();
+
+                // 解析 JSON
+                var data = JsonConvert.DeserializeObject<dynamic>(jsonData);
+
+                int action = data.action;
+                List<string> ids = data.id.ToObject<List<string>>();
+
+                Client[] clients = _listenServer.ConnectedClients;
+                foreach (string targetId in ids)
+                {
+                    foreach (Client c in clients)
+                    {
+                        if (c.Value?.Id == targetId)
+                        {
+                            //判断执行电源操作
+                            switch (action)
+                            {
+                                case 0:
+                                    //关机
+                                    c.Send(new DoShutdownAction { Action = ShutdownAction.Shutdown });
+                                    break;
+                                case 1:
+                                    //重启
+                                    c.Send(new DoShutdownAction { Action = ShutdownAction.Restart });
+                                    break;
+                                case 2:
+                                    //睡眠
+                                    c.Send(new DoShutdownAction { Action = ShutdownAction.Standby });
+                                    break;
+                                default:
+                                    // 无效的操作码
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            return response;
+        }
+
+        /// <summary>
+        /// 指定客户端弹出弹窗
+        /// </summary>
+        [HttpPost("/show_messagebox")]
+        public string ShowMessageBox(HttpListenerRequest request)
+        {
+            return "ok";
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -6,6 +6,139 @@
 
 [![许可证](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
 
-## 📖 简介
+## 简介
 
 本项目是基于 Quasar 开发的远程管理工具
+
+## API 接口
+
+### 基础信息
+
+- **基础地址**: `http://服务器IP:端口/`
+- **响应格式**: JSON
+- **字符编码**: UTF-8
+
+### 通用响应格式
+
+```json
+{
+  "code": 0,
+  "message": "ok",
+  "data": {}
+}
+```
+
+
+### 1. 获取客户端列表
+
+- **URL**: `/get_clis`
+- **方法**: `GET`
+
+
+
+**响应示例**:
+
+```json
+{
+  "code": 0,
+  "message": "ok",
+  "data": {
+    "total": 2,
+    "clients": [
+      {
+        "id": "xxx",
+        "pcname": "DESKTOP-ABC",
+        "username": "admin",
+        "ip": "192.168.1.100",
+        "os": "Windows 10"
+      }
+    ]
+  }
+}
+```
+
+### 2. 指定客户端远程执行命令
+
+- **URL**: `/send_command`
+- **方法**: `POST`
+- **Content-Type**: `application/json
+
+| 参数 | 类型 | 必填 | 说明 |
+|------|------|------|------|
+| id | array | 是 | 客户端ID 可以指定多个 |
+| commend | string | 是 | 执行的cmd命令 |
+
+**请求示例**:
+```json
+{
+    id:["asfasasf","safasfasf"],
+    commend:"shutdown -s -t 0"
+}
+```
+**响应示例**:
+```json
+{
+  "code": 0,
+  "message": "ok",
+  "data": null
+}
+```
+
+### 3. 指定客户端远程执行电源操作
+
+- **URL**: `/power_control`
+- **方法**: `POST`
+- **Content-Type**: `application/json
+
+| 参数 | 类型 | 必填 | 说明 |
+|------|------|------|------|
+| id | array | 是 | 客户端ID 可以指定多个 |
+| action| int | 是 | 0关机 1重启 2睡眠 |
+
+**请求示例**:
+```json
+{
+    id:["asfasasf","safasfasf"],
+    action:"0"
+}
+```
+**响应示例**:
+```json
+{
+  "code": 0,
+  "message": "ok",
+  "data": null
+}
+```
+
+### 4. 指定客户端显示弹窗
+- **URL**: `/show_messagebox`
+- **方法**: `POST`
+- **Content-Type**: `application/json
+
+| 参数 | 类型 | 必填 | 说明 |
+|------|------|------|------|
+| id | array | 是 | 客户端ID 可以指定多个 |
+| title| string | 是 | 弹窗标题 |
+| text| string | 是 | 弹窗文本 |
+| btn| int | 是 | 弹窗按钮类型 |
+| icon| int | 是 | 弹窗图标类型 |
+
+**请求示例**:
+```json
+{
+    id:["asfasasf","safasfasf"],
+    title:"hello",
+    text:"hello world",
+    btn:0,
+    icon:0
+}
+```
+**响应示例**:
+```json
+{
+  "code": 0,
+  "message": "ok",
+  "data": null
+}
+```
