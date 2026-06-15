b97f513e5971a3c2699bb5957ec86c225a44dd9f
ZhenPaiSkye <1053703868@qq.com>
2026-06-15 23:58:57 +0800

    杂项

--- Quasar.Server/Properties/AssemblyInfo.cs
+++ Quasar.Server/Properties/AssemblyInfo.cs
@@ -2,33 +2,22 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Quasar Server")]
-[assembly: AssemblyDescription("Remote Administration Tool")]
+// 程序集基本信息
+[assembly: AssemblyTitle("XINGYE 远程管理工具")]
+[assembly: AssemblyDescription("远程管理与监控系统")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Quasar")]
-[assembly: AssemblyCopyright("Copyright © MaxXor 2023")]
+[assembly: AssemblyProduct("XINGYE")]
+[assembly: AssemblyCopyright("Copyright © 2026 ZhenPaiSkye")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Server.Tests")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+// 单元测试可见性（如果有测试项目）
+[assembly: InternalsVisibleTo("XINGYE.Tests")]
+
+// COM 组件不可见
 [assembly: ComVisible(false)]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
+// 版本信息
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
